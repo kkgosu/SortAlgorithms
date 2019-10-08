18b24a3ec37e7976554ebfbfbbcea7c848be7cf6
kkgosu <kostya96.08@mail.ru>
2019-10-08 13:51:20 +0300

    implemented delete() method

--- src/udemy/trees/course/Tree.java
+++ src/udemy/trees/course/Tree.java
@@ -11,6 +11,32 @@ public class Tree {
         }
     }
 
+    public void delete(int value) {
+        root = delete(root, value);
+    }
+
+    private TreeNode delete(TreeNode subTreeRoot, int value) {
+        if (subTreeRoot == null) {
+            return null;
+        }
+
+        if (value < subTreeRoot.getData()) {
+            subTreeRoot.setLeftChild(delete(subTreeRoot.getLeftChild(), value));
+        } else if (value > subTreeRoot.getData()) {
+            subTreeRoot.setRightChild(delete(subTreeRoot.getRightChild(), value));
+        } else {
+            if (subTreeRoot.getLeftChild() == null) {
+                return subTreeRoot.getRightChild();
+            } else if (subTreeRoot.getRightChild() == null) {
+                return subTreeRoot.getLeftChild();
+            }
+
+            subTreeRoot.setData(subTreeRoot.getRightChild().min());
+            subTreeRoot.setRightChild(delete(subTreeRoot.getRightChild(), subTreeRoot.getData()));
+        }
+        return subTreeRoot;
+    }
+
     public void traverseInOrder() {
         if (root != null) {
             root.traverseInOrder();
